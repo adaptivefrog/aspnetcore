ccf41b0e82702cf953ecf9e5979c42aecf83bfb2
David Fowler <davidfowl@gmail.com>
2022-07-11 19:51:48 -0700

    Remove extra usings

--- src/Mvc/Mvc.Core/src/Routing/ActionEndpointFactory.cs
+++ src/Mvc/Mvc.Core/src/Routing/ActionEndpointFactory.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -14,7 +13,6 @@ using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Patterns;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Internal;
 
 namespace Microsoft.AspNetCore.Mvc.Routing;
 
